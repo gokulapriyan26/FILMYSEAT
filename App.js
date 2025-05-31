ff2ff21934845518364fb691c3c2c0908dbddea8
gokulapriyan26 <gokulapriyan26@gmail.com>
2025-05-31 11:35:41 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
 
 function App() {
-  return <div>Welcome to FILMYSEAT!</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
