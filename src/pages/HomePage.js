7bcf5dbd02b250dce337546e6afd896145df61aa
gokulapriyan26 <gokulapriyan26@gmail.com>
2025-05-31 11:53:33 +0800

    Create HomePage.js

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Welcome to FILMYSEAT</h1>
+      <p>Book your tickets now at Abreeza Ayala Cinemas</p>
+    </div>
+  );
+}
+
+export default HomePage;
